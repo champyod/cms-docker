f41f38fd5b20904ca1de04f84ffec1aa98843bfa
CCYod <champyodsakon@gmail.com>
2025-10-08 22:58:54 +0700

    manual add user

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 set -e
 
-# Set PostgreSQL user permissions for CMS
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    ALTER SCHEMA public OWNER TO $POSTGRES_USER;
-    GRANT SELECT ON pg_largeobject TO $POSTGRES_USER;
+# Create user and database if they don't exist
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${POSTGRES_USER}') THEN
+            CREATE ROLE ${POSTGRES_USER} WITH LOGIN PASSWORD '${POSTGRES_PASSWORD}';
+        END IF;
+    END
+    \$\$;
+
+    -- Check if the database exists before trying to create it
+    -- Note: This is a workaround since CREATE DATABASE IF NOT EXISTS is not available in all versions
+    SELECT 'CREATE DATABASE ${POSTGRES_DB}'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB}');
+
+    -- Run the create database command (it will be empty if the DB exists)
+    \gexec
+
+    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
 EOSQL
 
-echo "Database initialized with proper permissions for CMS"
+# Set specific permissions required by CMS
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    ALTER SCHEMA public OWNER TO ${POSTGRES_USER};
+    GRANT SELECT ON pg_largeobject TO ${POSTGRES_USER};
+EOSQL
+
+echo "Database and user configured for CMS"
